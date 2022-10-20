7d8563b6bc31193b3f3c36902ac97fac3dfd65b2
IhssanTsouli <ihssantsouli@gmail.com>
2022-10-20 08:41:24 -0700

    Query Builder and Migration

--- Database/app/Http/Controllers/Controller.php
+++ Database/app/Http/Controllers/Controller.php
@@ -6,10 +6,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    
-}
+    public function index()
+    {
+        $user = DB::table('promotion')->get();
+ 
+        return view('index',compact('user'));
+    }
 
+    public function FindId($id)
+    {
+        $user = DB::table('promotion')->where("id",$id)->get();
+        
+        return view('userId',compact('user'));
+    }
+
+}
